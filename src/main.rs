2d2f6b5288b4755a600117c2cf81b97865e6a712
Weston Pace <weston.pace@gmail.com>
2024-01-15 07:22:05 -0800

    WIP

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,8 @@ async fn main() {
     for thread_idx in 0..128 {
         let client = &client;
         parent_tasks.push( async move {
-            let mut inner_tasks = Vec::with_capacity(128);
             sleep(Duration::from_millis(1)).await;
-            for run_idx in 0..128 {
+            stream::iter(0..128).map(move |run_idx| {
                 let idx = thread_idx * 128 + run_idx;
                 let start = CHUNK_SIZE * idx;
                 let end = start + CHUNK_SIZE;
@@ -27,20 +26,19 @@ async fn main() {
                     client
                     .get("https://storage.googleapis.com/weston-public-test/weston/data/d3144d84-b8e5-4b9a-bc19-9ca64dabe0fd.lance")
                     .header("Range", range).send().and_then(|rsp| rsp.bytes().and_then(|bytes| std::future::ready(Ok(bytes.len()))));
-                inner_tasks.push(fut);
-            }
-            inner_tasks
+                tokio::task::spawn(fut)
+            })
         });
     }
 
     let mut counter = 0;
     let mut stream = stream::iter(parent_tasks)
         .buffered(8)
-        .flat_map(|item| stream::iter(item))
+        .flatten()
         .buffered(8);
     while let Some(res) = stream.next().await {
         counter += 1;
-        match res {
+        match res.unwrap() {
             Ok(bytes_received) => {
                 println!("{}: Downloaded {} bytes", counter, bytes_received);
             }
